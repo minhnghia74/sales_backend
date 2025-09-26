5dd61d3b1366b2a94401f7ddfe6066ffb7263f46
Trần Minh Nghĩa <tranminh.nghia@petronas.com>
2025-09-27 01:29:41 +0700

    add ssl connection

--- db.js
+++ db.js
@@ -5,7 +5,8 @@ const pool = new Pool({
   host: "dpg-d3bc8bffte5s739kshg0-a.oregon-postgres.render.com",      // chạy local
   database: "sales_db_d5y8",   // tên database
   password: "dPcXrK2CdxVGZors9q9w73S97liqPOTI",     // password PostgreSQL
-  port: 5432,             // port mặc định
+  port: 5432,         // port mặc định
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
